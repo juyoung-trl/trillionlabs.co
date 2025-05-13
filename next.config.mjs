b21a663a7ce403e24c3c1a705d725ae14a3a81dc
Juyoung Suk <juyoung.suk@trillionlabs.co>
2025-05-13 04:32:14 +0000

    Update progress

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/trillionlabs.co' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/trillionlabs.co/' : '',
 }
 
 export default nextConfig
